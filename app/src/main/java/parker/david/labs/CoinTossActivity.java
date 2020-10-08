c50186eaab0a5b3f615dc5b08fdaf38b9a9dfe87
Harve <harvey290@hotmail.co.uk>
2020-10-08 16:34:04 +0100

    Lab 2

---
+++ app/src/main/java/parker/david/labs/CoinTossActivity.java
@@ -0,0 +1,54 @@
+package parker.david.labs;
+
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.util.Log;
+
+import java.util.Random;
+
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class CoinTossActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coin_toss);
+        Log.i("Activity Lifecycle",  "onCreate");
+
+        Bundle extras = getIntent().getExtras();
+        String name = extras.getString("ExtraName");
+        Toast.makeText(getApplicationContext(),"This is the extra string that we passed in: " + name,Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i( "Activity Lifecycle", "onPause");
+    }
+
+    @Override
+    protected  void onResume() {
+        super.onResume();
+        Log.i("Activity Lifecycle", "onResume");
+        TextView coinTossView = findViewById(R.id.coinTossView);
+
+        String result = getCoinToss();
+
+        coinTossView.setText(result);
+    }
+
+    private String getCoinToss() {
+        Random random = new Random();
+        if(random.nextBoolean()){
+            return "Heads!";
+        }
+        return  "Tails!";
+    }
+}
--- app/src/main/java/parker/david/labs/MainActivity.java
+++ app/src/main/java/parker/david/labs/MainActivity.java
@@ -2,7 +2,11 @@ package parker.david.labs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openCoinToss(View view) {
+        Intent openCoinTossIntent = new Intent(getApplicationContext(),CoinTossActivity.class);
+        openCoinTossIntent.putExtra("ExtraName","ExtraValue");
+        startActivity(openCoinTossIntent);
+    }
+
+    public void  openUrl(View view) {
+        Intent openImplicitIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.goparker.com"));
+        startActivity(openImplicitIntent);
     }
 }
